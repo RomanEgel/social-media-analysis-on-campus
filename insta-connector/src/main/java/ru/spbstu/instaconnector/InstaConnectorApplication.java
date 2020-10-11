5faa02ff410ba6fc26a37068d5c028910f913062
regel <roman.egel@firstlinesoftware.com>
2020-10-11 13:56:29 +0300

    init

---
+++ insta-connector/src/main/java/ru/spbstu/instaconnector/InstaConnectorApplication.java
@@ -0,0 +1,13 @@
+package ru.spbstu.instaconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstaConnectorApplication.class, args);
+    }
+
+}
---
+++ insta-connector/src/test/java/ru/spbstu/instaconnector/InstaConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spbstu.instaconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
